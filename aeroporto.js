344f147764aeb473d16286f5f229336bfab5080c
Aristófanes Paulino <121912760+aristofanesps@users.noreply.github.com>
2024-01-30 01:19:57 -0300

    Update aeroporto.js

--- aeroporto.js
+++ aeroporto.js
@@ -1,7 +1,6 @@
 
-const dominioAtual = window.location.hostname;
-if(dominioAtual==='https://aristofanesps.github.io/ari/aeroporto.html'{
-  function obterDataHoraFormatada() {
+
+  function wobterDataHoraFormatada() {
     var data = new Date();
     var options = {
       year: "numeric",
@@ -14,31 +13,31 @@ if(dominioAtual==='https://aristofanesps.github.io/ari/aeroporto.html'{
     };
 
     // Formate a data e hora
-    var dataHoraFormatada = data.toLocaleString("pt-BR", options);
+    var wdataHoraFormatada = data.toLocaleString("pt-BR", options);
     return dataHoraFormatada;
   }
 
   // Seu JavaScript existente
   // ...
   // Função para carregar o estoque do armazenamento local
-  function carregarEstoqueDoLocalStorage() {
+  function wcarregarEstoqueDoLocalStorage() {
     var estoqueSalvo = localStorage.getItem("estoque");
 
     if (estoqueSalvo) {
       estoque = JSON.parse(estoqueSalvo);
-      exibirEstoque();
-      calcularSomaEstoque();
+      wexibirEstoque();
+      wcalcularSomaEstoque();
     }
   }
 
   // Função para salvar o estoque no armazenamento local
-  function salvarEstoqueNoLocalStorage() {
+  function wsalvarEstoqueNoLocalStorage() {
     localStorage.setItem("estoque", JSON.stringify(estoque));
   }
 
   // Função para carregar os dados do scroll-container do armazenamento local
-  function carregarDadosDoLocalStorage() {
-    var dadosSalvos = localStorage.getItem("dadosScrollContainer");
+  function wcarregarDadosDoLocalStorage() {
+    var wdadosSalvos = localStorage.getItem("dadosScrollContainer");
 
     if (dadosSalvos) {
       document.getElementById("recebimentosTbody").innerHTML = dadosSalvos;
@@ -46,8 +45,8 @@ if(dominioAtual==='https://aristofanesps.github.io/ari/aeroporto.html'{
   }
 
   // Função para salvar os dados do scroll-container no armazenamento local
-  function salvarDadosNoLocalStorage() {
-    var dadosScrollContainer = document.getElementById("recebimentosTbody").innerHTML;
+  function wsalvarDadosNoLocalStorage() {
+    var wdadosScrollContainer = document.getElementById("recebimentosTbody").innerHTML;
     localStorage.setItem("dadosScrollContainer", dadosScrollContainer);
   }
 
@@ -72,38 +71,38 @@ if(dominioAtual==='https://aristofanesps.github.io/ari/aeroporto.html'{
 };
 
   // Carregue o estoque e os dados do scroll-container ao inicializar a página
-  carregarEstoqueDoLocalStorage();
-  carregarDadosDoLocalStorage();
+  wcarregarEstoqueDoLocalStorage();
+  wcarregarDadosDoLocalStorage();
 
-  function registrarRecebimento() {
-    var productName = document.getElementById("productName").value;
-    var receivedByName = document.getElementById("receivedByName").value;
-    var matricula = document.getElementById("matricula").value;
-    var currentDate = obterDataHoraFormatada(); // Obtém a data e hora atuais
+  function wregistrarRecebimento() {
+    var wproductName = document.getElementById("productName").value;
+    var wreceivedByName = document.getElementById("receivedByName").value;
+    var wmatricula = document.getElementById("matricula").value;
+    var wcurrentDate = obterDataHoraFormatada(); // Obtém a data e hora atuais
 
     if (productName && receivedByName && matricula) {
-      var tableBody = document.getElementById("recebimentosTbody");
+      var wtableBody = document.getElementById("recebimentosTbody");
 
       if (estoque[productName] > 0) {
-        var newRow = tableBody.insertRow(tableBody.rows.length);
-        var cell1 = newRow.insertCell(0);
-        var cell2 = newRow.insertCell(1);
-        var cell3 = newRow.insertCell(2);
-        var cell4 = newRow.insertCell(3);
+        var wnewRow = tableBody.insertRow(tableBody.rows.length);
+        var wcell1 = newRow.insertCell(0);
+        var wcell2 = newRow.insertCell(1);
+        var wcell3 = newRow.insertCell(2);
+        var wcell4 = newRow.insertCell(3);
 
-        cell1.innerHTML = matricula;
-        cell2.innerHTML = receivedByName;
-        cell3.innerHTML = productName;
-        cell4.innerHTML = currentDate;
+        wcell1.innerHTML = matricula;
+        wcell2.innerHTML = receivedByName;
+        wcell3.innerHTML = productName;
+        wcell4.innerHTML = currentDate;
 
         estoque[productName]--;
-        exibirEstoque();
-        salvarDadosNoLocalStorage();
+        wexibirEstoque();
+        wsalvarDadosNoLocalStorage();
 
         document.getElementById("receivedByName").value = "";
         document.getElementById("matricula").value = "";
       } else {
-        alert("Estoque do produto " + productName + " esgotado!");
+        alert("Estoque do produto " + wproductName + " esgotado!");
       }
     } else {
       alert("Por favor, preencha todos os campos.");
@@ -125,8 +124,8 @@ if(dominioAtual==='https://aristofanesps.github.io/ari/aeroporto.html'{
     document.getElementById("estoqueInicialCapa").innerText = estoque["Capa de Chuva"];
   }
 
-function calcularSomaEstoque() {
-    var somaEstoque = 0;
+function wcalcularSomaEstoque() {
+    var wsomaEstoque = 0;
 
     for (var item in estoque) {
         if (estoque.hasOwnProperty(item)) {
@@ -134,7 +133,7 @@ function calcularSomaEstoque() {
     }
 }
 
-  var elemento=document.getElementById("totalEstoque");
+  var welemento=document.getElementById("totalEstoque");
   if (elemento) { // Define o estilo do elemento para ocultar (display: none) elemento.style.display = 'none'; 
 }
 }
@@ -148,69 +147,69 @@ function calcularSomaEstoque() {
 
   // Atualize a tabela e o estoque ao carregar a página
   window.onload = function () {
-    carregarEstoqueDoLocalStorage();
-    carregarDadosDoLocalStorage();
-    exibirEstoque();
-    calcularSomaEstoque();
+    wcarregarEstoqueDoLocalStorage();
+    wcarregarDadosDoLocalStorage();
+    wexibirEstoque();
+    wcalcularSomaEstoque();
   };
 
-  function alterarEstoqueInicial() {
-    var novoEstoqueLatex = parseInt(prompt("Digite o novo valor para o estoque de Luva de Látex (amarela):"));
-    var novoEstoqueNitrilica = parseInt(prompt("Digite o novo valor para o estoque de Luva Nitrílica (verde):"));
-    var novoEstoquePigmentada = parseInt(prompt("Digite o novo valor para o estoque de Luva Pigmentada (algodão):"));
-    var novoEstoqueNitrilon = parseInt(prompt("Digite o novo valor para o estoque de Luva Nitrilon:"));
-    var novoEstoqueAvental = parseInt(prompt("Digite o novo valor para o estoque de Avental de Napa (preto):"));
-    var novoEstoqueColete = parseInt(prompt("Digite o novo valor para o estoque de Colete Refletivo:"));
-    var novoEstoqueMascara = parseInt(prompt("Digite o novo valor para o estoque de Máscara PFF2:"));
-    var novoEstoqueProtetor = parseInt(prompt("Digite o novo valor para o estoque de Protetor Auricular:"));
-    var novoEstoqueOculos = parseInt(prompt("Digite o novo valor para o estoque de Óculos de Segurança:"));
-    var novoEstoqueBone = parseInt(prompt("Digite o novo valor para o estoque de Boné:"));
-    var novoEstoqueBoneArabe = parseInt(prompt("Digite o novo valor para o estoque de Boné Árabe:"));
-    var novoEstoqueCapa = parseInt(prompt("Digite o novo valor para o estoque de Capa de Chuva:"));
+  function walterarEstoqueInicial() {
+    var wnovoEstoqueLatex = parseInt(prompt("Digite o novo valor para o estoque de Luva de Látex (amarela):"));
+    var wnovoEstoqueNitrilica = parseInt(prompt("Digite o novo valor para o estoque de Luva Nitrílica (verde):"));
+    var wnovoEstoquePigmentada = parseInt(prompt("Digite o novo valor para o estoque de Luva Pigmentada (algodão):"));
+    var wnovoEstoqueNitrilon = parseInt(prompt("Digite o novo valor para o estoque de Luva Nitrilon:"));
+    var wnovoEstoqueAvental = parseInt(prompt("Digite o novo valor para o estoque de Avental de Napa (preto):"));
+    var wnovoEstoqueColete = parseInt(prompt("Digite o novo valor para o estoque de Colete Refletivo:"));
+    var wnovoEstoqueMascara = parseInt(prompt("Digite o novo valor para o estoque de Máscara PFF2:"));
+    var wnovoEstoqueProtetor = parseInt(prompt("Digite o novo valor para o estoque de Protetor Auricular:"));
+    var wnovoEstoqueOculos = parseInt(prompt("Digite o novo valor para o estoque de Óculos de Segurança:"));
+    var wnovoEstoqueBone = parseInt(prompt("Digite o novo valor para o estoque de Boné:"));
+    var wnovoEstoqueBoneArabe = parseInt(prompt("Digite o novo valor para o estoque de Boné Árabe:"));
+    var wnovoEstoqueCapa = parseInt(prompt("Digite o novo valor para o estoque de Capa de Chuva:"));
 
 
 
     // Verifique se os valores digitados são números válidos
-    if (!isNaN(novoEstoqueLatex) && !isNaN(novoEstoqueNitrilica) && !isNaN(novoEstoquePigmentada) && !isNaN(novoEstoqueNitrilon) && !isNaN(novoEstoqueAvental) && !isNaN(novoEstoqueColete) && !isNaN(novoEstoqueMascara) && !isNaN(novoEstoqueProtetor) && !isNaN(novoEstoqueOculos) && !isNaN(novoEstoqueBone) && !isNaN(novoEstoqueBoneArabe) && !isNaN(novoEstoqueCapa)) {
-      estoque["Luva de Látex (amarela)"] = novoEstoqueLatex;
-      estoque["Luva Nitrílica (verde)"] = novoEstoqueNitrilica;
-      estoque["Luva Pigmentada (algodão)"] = novoEstoquePigmentada;
-      estoque["Luva Nitrilon"] = novoEstoqueNitrilon;
-      estoque["Avental de Napa (preto)"] = novoEstoqueAvental;
-      estoque["Colete Refletivo"] = novoEstoqueColete;
-      estoque["Máscara PFF2 (azul)"] = novoEstoqueMascara;
-      estoque["Protetor Auricular"] = novoEstoqueProtetor;
-      estoque["Óculos de Segurança"] = novoEstoqueOculos;
-      estoque["Boné"] = novoEstoqueBone;
-      estoque["Boné Árabe"] = novoEstoqueBoneArabe;
-      estoque["Capa de Chuva"] = novoEstoqueCapa;
+    if (!isNaN(wnovoEstoqueLatex) && !isNaN(wnovoEstoqueNitrilica) && !isNaN(wnovoEstoquePigmentada) && !isNaN(wnovoEstoqueNitrilon) && !isNaN(wnovoEstoqueAvental) && !isNaN(wnovoEstoqueColete) && !isNaN(wnovoEstoqueMascara) && !isNaN(wnovoEstoqueProtetor) && !isNaN(wnovoEstoqueOculos) && !isNaN(wnovoEstoqueBone) && !isNaN(wnovoEstoqueBoneArabe) && !isNaN(wnovoEstoqueCapa)) {
+      estoque["Luva de Látex (amarela)"] = wnovoEstoqueLatex;
+      estoque["Luva Nitrílica (verde)"] = wnovoEstoqueNitrilica;
+      estoque["Luva Pigmentada (algodão)"] = wnovoEstoquePigmentada;
+      estoque["Luva Nitrilon"] = wnovoEstoqueNitrilon;
+      estoque["Avental de Napa (preto)"] = wnovoEstoqueAvental;
+      estoque["Colete Refletivo"] = wnovoEstoqueColete;
+      estoque["Máscara PFF2 (azul)"] = wnovoEstoqueMascara;
+      estoque["Protetor Auricular"] = wnovoEstoqueProtetor;
+      estoque["Óculos de Segurança"] = wnovoEstoqueOculos;
+      estoque["Boné"] = wnovoEstoqueBone;
+      estoque["Boné Árabe"] = wnovoEstoqueBoneArabe;
+      estoque["Capa de Chuva"] = wnovoEstoqueCapa;
 
 
       // Exiba o estoque atualizado
-      exibirEstoque();
-      calcularSomaEstoque();
+      wexibirEstoque();
+      wcalcularSomaEstoque();
 
       // Salve o estoque no armazenamento local
-      salvarEstoqueNoLocalStorage();
+      wsalvarEstoqueNoLocalStorage();
     } else {
       alert("Por favor, digite valores numéricos válidos.");
     }
   }
 
-  function acessarAlteracaoEstoque() {
-    var senha = prompt("Digite a senha para acessar:");
+  function wacessarAlteracaoEstoque() {
+    var wsenha = prompt("Digite a senha para acessar:");
 
     // Verifique se a senha é correta (substitua 'suaSenhaAqui' pela senha desejada)
-    if (senha === "1234") {
-      alterarEstoqueInicial();
+    if (wsenha === "1234") {
+      walterarEstoqueInicial();
     } else {
       alert("Senha incorreta. Acesso negado.");
     }
   }
 
-  function limparDadosLocalStorage() {
+  function wlimparDadosLocalStorage() {
     // Carregar o estoque do LocalStorage antes de limpar os dados do scroll-container
-    carregarEstoqueDoLocalStorage();
+    wcarregarEstoqueDoLocalStorage();
 
     // Remover apenas os dados do scroll-container
     localStorage.removeItem("dadosScrollContainer");
@@ -219,7 +218,7 @@ function calcularSomaEstoque() {
   }
 
   // Função para filtrar os dados na tabela
-  function filtrarDados(inputId, columnIndex) {
+  function wfiltrarDados(inputId, columnIndex) {
     var input, filter, table, tbody, tr, td, i, txtValue;
     input = document.getElementById(inputId);
     filter = input.value.toUpperCase();
@@ -239,14 +238,14 @@ function calcularSomaEstoque() {
       }
     }
   }
-function limparDadosLocalStorage() {
+function wlimparDadosLocalStorage() {
 // Solicitar a senha ao usuário
-var senhaInserida = prompt("Digite a senha para limpar os dados:");
+var wsenhaInserida = prompt("Digite a senha para limpar os dados:");
 
 // Verificar se a senha está correta
-if (senhaInserida === "1234") {
+if (wsenhaInserida === "1234") {
     // Carregar o estoque do LocalStorage antes de limpar os dados do scroll-container
-    carregarEstoqueDoLocalStorage();
+    wcarregarEstoqueDoLocalStorage();
 
     // Remover apenas os dados do scroll-container
     localStorage.removeItem("dadosScrollContainer");
@@ -258,32 +257,32 @@ if (senhaInserida === "1234") {
 }
 
 // Aqui deve ser colocado o restante do seu JavaScript
-function salvarEstoqueNoLocalStorage() {
+function wsalvarEstoqueNoLocalStorage() {
   localStorage.setItem("estoque", JSON.stringify(estoque));
 }
 
 // Função para carregar os dados da tabela de estoque do armazenamento local
-function carregarEstoqueDoLocalStorage() {
-  var estoqueSalvo = localStorage.getItem("estoque");
+function wcarregarEstoqueDoLocalStorage() {
+  var westoqueSalvo = localStorage.getItem("estoque");
 
-  if (estoqueSalvo) {
+  if (westoqueSalvo) {
     estoque = JSON.parse(estoqueSalvo);
-    exibirEstoque();
-    calcularSomaEstoque();
+    wexibirEstoque();
+    wcalcularSomaEstoque();
   }
 }
 
 // Adicione esta função para salvar os dados da tabela de estoque ao fechar a página
 window.onbeforeunload = function() {
-  salvarEstoqueNoLocalStorage();
+  wsalvarEstoqueNoLocalStorage();
 };
 
 // Chame a função carregarEstoqueDoLocalStorage ao carregar a página
 window.onload = function() {
-  carregarEstoqueDoLocalStorage();
-  carregarDadosDoLocalStorage();
-  exibirEstoque();
-  calcularSomaEstoque();
+  wcarregarEstoqueDoLocalStorage();
+  wcarregarDadosDoLocalStorage();
+  wexibirEstoque();
+  wcalcularSomaEstoque();
 };
 
   
@@ -293,9 +292,6 @@ window.onload = function() {
  // Função para obter a data e hora formatadas
 
   
-}else{
-  alert("ola mundo")
-}
 
   
 
